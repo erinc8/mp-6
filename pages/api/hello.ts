cae205cb78f49149404e1dbd7b46177f5104f8db
erinc8 <erinc8@bu.edu>
2025-05-01 00:08:51 -0400

    test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,3 +1,6 @@
-export default function handler(req, res) {
+// pages/api/hello.ts
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
     res.status(200).json({ message: 'API is working!' });
 }
