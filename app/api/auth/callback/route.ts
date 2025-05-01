6788ffd0af4ba80621bf101feaec856efaad2129
erinc8 <erinc8@bu.edu>
2025-04-30 22:03:47 -0400

    made some small edits

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -13,9 +13,9 @@ export async function GET(req: NextRequest) {
     const tokenEndpoint = 'https://oauth2.googleapis.com/token';
     const payload = new URLSearchParams({
         code,
-        client_id: process.env.GOOGLE_CLIENT_ID!,
+        client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
         client_secret: process.env.GOOGLE_CLIENT_SECRET!,
-        redirect_uri: process.env.REDIRECT_URI!,
+        redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_URI!,
         grant_type: 'authorization_code',
     });
 
