51ebc538341d95704a1a68a7ee9e969f09aa4f82
erinc8 <erinc8@bu.edu>
2025-05-01 15:02:32 -0400

    small chages

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,14 @@
-import { handlers } from "@/auth";
-export const { GET, POST } = handlers;
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET || process.env.AUTH_SECRET,
+});
+
+export { handler as GET, handler as POST };
