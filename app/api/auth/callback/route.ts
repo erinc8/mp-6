7815fdaf5533e1bebaed6b90adbe9e133e6f038f
erinc8 <erinc8@bu.edu>
2025-04-30 21:50:58 -0400

    added another line in route.ts

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+export const runtime = 'nodejs';
 
 export async function GET(req: NextRequest) {
     const { searchParams } = new URL(req.url);
